7031d5ae5c970af076b9db95f31d05a553c66d6b
Sean Wines <seanwinesdev@gmail.com>
2016-07-20 21:35:49 -0700

    JS added to portfolio page

--- js/script.js
+++ js/script.js
@@ -3,41 +3,42 @@ $remove_button = $('.remove');
 $hero = $('.hero');
 $about = $('.about');
 $about_p = $('.hero p');
+$index_body = $('#index');
 
 // Show about div. Hide about button on hero section
 $about_button.on('click', function() {
   $(this).hide();
-  $hero.animate({top: 100});
-  $about.show().animate({top: 140}, 'slow');
   $about_p.css('margin-bottom', '0');
+  $index_body.css('overflow', 'scroll');
 
     if ($(window).width() <= 520) {
       $hero.hide();
       $about.show().animate({top: 10}, 'slow');
-    }
-    if ($(window).width() <= 1300) {
+    } else if ($(window).width() >= 1400) {
+      $hero.animate({top: 100});
+      $about.show().animate({top: 140}, 'slow');
+    } else {
       $hero.animate({top: 10});
-      $about.show().animate({top: 20}, 'slow');
+      $about.show().animate({top: 40}, 'slow');
     }
-
 });
 
 // hide about div. Show about button on hero section
 $remove_button.on('click', function() {
-  $hero.animate({top: 180});
-  $about.animate({top: 1000}, 'slow').fadeOut();
   $about_button.fadeIn();
   $about_p.css('margin-bottom', '10px');
 
     if ($(window).width() <= 520) {
       $hero.fadeIn();
-      $hero.animate({top: 100});
-    }
-    if ($(window).width() <= 1300) {
+      $hero.animate({top: 60});
+      $about.hide();
+    } else if ($(window).width() >= 1400) {
+      $hero.animate({top: 180});
+      $about.hide();
+    } else {
       $hero.animate({top: 100});
       $about.animate({top: 1000}, 'slow').fadeOut();
     }
-
 });
 
 // pag nav arrows
@@ -75,8 +76,16 @@ $('.hide_nav').on('click', function() {
   $('.top_bar').animate({left: 3000});
 })
 
-$(window).resize(function(){
-  if ($(window).width() <= 520) {
 
-  }
-});
+
+function loopDivs() {
+
+  $('.display_responsive').fadeIn().delay(5000).fadeOut();
+
+  $('.display_toolbelt').delay(5800).fadeIn().delay(5000).fadeOut();
+
+  $('.display_environment').delay(12000).fadeIn().delay(5000).fadeOut(loopDivs);
+
+}
+
+loopDivs();
