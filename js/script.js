4bba01c9b3c7e6cc4b9c894e0eb5edd3e5c0ae3e
Sean Wines <seanwinesdev@gmail.com>
2016-08-07 16:46:26 -0700

    Added mini-projects and worked on portfolio page

--- js/script.js
+++ js/script.js
@@ -6,88 +6,120 @@ var
   $about_p = $('.hero p'),
   $index_body = $('#index');
 
-// Show about div. Hide about button on hero section
-$about_button.on('click', function() {
-  $(this).hide();
-  $about_p.css('margin-bottom', '0');
-  $index_body.css('overflow', 'scroll');
+function loopDivs() {
+  $('.display_responsive').fadeIn().delay(10000).fadeOut();
+  $('.display_toolbelt').delay(10500).fadeIn('slow').delay(10000).fadeOut('slow', loopDivs);
+}
+
+$(function() {
+
+  loopDivs();
+
+  // Show about div. Hide about button on hero section
+  $about_button.on('click', function() {
+    $(this).hide();
+    $about_p.css('margin-bottom', '0');
+    $index_body.css('overflow', 'scroll');
 
     if ($(window).width() <= 520) {
       $hero.hide();
-      $about.show().animate({top: 10}, 'slow');
+      $about.show().animate({
+        top: 10
+      }, 'slow');
     } else if ($(window).width() >= 1400) {
-      $hero.animate({top: 100});
-      $about.show().animate({top: 140}, 'slow');
+      $hero.animate({
+        top: 100
+      });
+      $about.show().animate({
+        top: 140
+      }, 'slow');
     } else {
-      $hero.animate({top: 10});
-      $about.show().animate({top: 40}, 'slow');
+      $hero.animate({
+        top: 10
+      });
+      $about.show().animate({
+        top: 40
+      }, 'slow');
     }
-});
+  });
 
-// hide about div. Show about button on hero section
-$remove_button.on('click', function() {
-  $about_button.fadeIn();
-  $about_p.css('margin-bottom', '10px');
+  // Hide about div. Show hero button 
+  $remove_button.on('click', function() {
+    $about_button.fadeIn();
+    $about_p.css('margin-bottom', '10px');
 
     if ($(window).width() <= 520) {
       $hero.fadeIn();
-      $hero.animate({top: 60});
+      $hero.animate({
+        top: 60
+      });
       $about.hide();
     } else if ($(window).width() >= 1400) {
-      $hero.animate({top: 180});
+      $hero.animate({
+        top: 180
+      });
       $about.hide();
     } else {
-      $hero.animate({top: 100});
-      $about.animate({top: 1000}, 'slow').fadeOut();
+      $hero.animate({
+        top: 100
+      });
+      $about.animate({
+        top: 1000
+      }, 'slow').fadeOut();
     }
-});
+  });
 
-// pag nav arrows
-// RIGHT
-$('.contact_page span, .port_page span').mouseover(function() {
-  $(this).fadeIn().css('color', '#fff');
-  $('.port_display, .contact_display').fadeIn();
+  // Top bar toggle show
+  $('.show_nav').on('click', function() {
+    $(this).fadeOut('fast');
+    $('.hide_nav').fadeIn();
+    $('.top_bar').animate({
+      left: 0
+    });
+  })
+
+  // Top bar toggle hide
+  $('.hide_nav').on('click', function() {
+    $(this).fadeOut();
+    $('.show_nav').fadeIn();
+    $('.top_bar').animate({
+      left: 3000
+    });
+  })
+
+  // PAGE NAV ARROWS
+  // RIGHT
+  $('.contact_page span, .port_page span').mouseover(function() {
+      $(this).fadeIn().css('color', '#fff');
+      $('.port_display, .contact_display').fadeIn();
+    })
+    .mouseout(function() {
+      $(this).css('color', 'rgba(255, 255, 255, 0.75)');
+      $('.port_display, .contact_display').fadeOut();
+    });
+
+  // LEFT
+  $('.about_page span, .port_page_left span').mouseover(function() {
+      $(this).fadeIn().css('color', 'rgba(255, 255, 255, 0.75)');
+      $('.about_display, .port_display_left').fadeIn();
+    })
+    .mouseout(function() {
+      $(this).css('color', 'rgba(255, 255, 255, 0.65)');
+      $('.about_display, .port_display_left').fadeOut();
+    });
+
+  var distance;
+  if ($(window).width() > 1400) {
+    distance = 700;
+  } else {
+    distance = 550;
+  }
+  $('.down_arrow').fadeIn('slow').animate({
+    top: distance
+  }).fadeOut('slow');
+
+  $('.work').hover(function() {
+    $('.work i').fadeIn();
+  })
+
 })
-.mouseout(function() {
-  $(this).css('color', 'rgba(255, 255, 255, 0.75)');
-  $('.port_display, .contact_display').fadeOut();
-});
-
-// LEFT
-$('.about_page span, .port_page_left span').mouseover(function() {
-  $(this).fadeIn().css('color', 'rgba(255, 255, 255, 0.75)');
-  $('.about_display, .port_display_left').fadeIn();
-})
-.mouseout(function() {
-  $(this).css('color', 'rgba(255, 255, 255, 0.65)');
-  $('.about_display, .port_display_left').fadeOut();
-});
-
-// top bar toggle open
-$('.show_nav').on('click', function() {
-  $(this).fadeOut('fast');
-  $('.hide_nav').fadeIn();
-  $('.top_bar').animate({left: 0});
-})
-
-// top bar toggle hide
-$('.hide_nav').on('click', function() {
-  $(this).fadeOut();
-  $('.show_nav').fadeIn();
-  $('.top_bar').animate({left: 3000});
-})
-
-// Flash down arrow on portfolio page
-$('.down_arrow').fadeIn('slow').animate({top: 700}).fadeOut('slow');
-
-function loopDivs() {
-
-  $('.display_responsive').fadeIn().delay(10000).fadeOut();
-
-  $('.display_toolbelt').delay(10500).fadeIn('slow').delay(10000).fadeOut('slow', loopDivs);
-
-  //$('.display_environment').delay(11500).fadeIn('slow').delay(5000).fadeOut(loopDivs);
-
-}
-
-loopDivs();
