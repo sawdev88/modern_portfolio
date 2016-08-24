c3198deb296ad9dc1a32e79b1715bc12bc790ccd
Sean Wines <seanwinesdev@gmail.com>
2016-08-24 08:17:21 -0700

    Refactored js

--- js/script.js
+++ js/script.js
@@ -1,137 +1,151 @@
-let $aboutButton = $('.about-button'),
-    $removeButton = $('.remove'),
-    $hero = $('.hero'),
-    $about = $('.about'),
-    $aboutP = $('.hero p'),
-    $indexBody = $('#index'),
-    $hideNav = $('.hide-nav'),
-    $showNav = $('.show-nav'),
-    $topBar = $('.top-bar');
-
-function loopDivImgs() {
-    $('.display-responsive').fadeIn().delay(10000).fadeOut();
-    $('.display-toolbelt').delay(10500).fadeIn('slow').delay(10000).fadeOut('slow', loopDivImgs);
-};
-
 $(function() {
 
-    // Recursion
-    loopDivImgs();
+  // Loop img side section on portfolio page
+  var loopDivImgs = function() {
+    $('.display-responsive').fadeIn().delay(10000).fadeOut();
+    $('.display-toolbelt').delay(10500).fadeIn('slow').delay(10000).fadeOut('slow', loopDivImgs);
+  };
 
-    // Show about div. Hide about button on hero section
+  loopDivImgs();
+
+  // Show about div. Hide about button on hero section
+  var toggleAboutSection = function () {
+    var $aboutButton = $('.about-button'),
+        $removeButton = $('.remove'),
+        $aboutP = $('.hero p'),
+        $indexBody = $('#index'),
+        $hero = $('.hero'),
+        $about = $('.about');
+
     $aboutButton.on('click', function() {
-        $(this).hide();
-        $aboutP.css('margin-bottom', '0');
-        $indexBody.css('overflow', 'scroll');
+      $(this).hide();
+      $aboutP.css('margin-bottom', '0');
+      $indexBody.css('overflow', 'scroll');
 
-        if ($(window).width() <= 520) {
-            $hero.hide();
-            $about.show().animate({
-                top: 10
-            }, 'slow');
-        } else if ($(window).width() >= 1400) {
-            $hero.animate({
-                top: 100
-            });
-            $about.show().animate({
-                top: 140
-            }, 'slow');
+      if ($(window).width() <= 520) {
+          $hero.hide();
+          $about.show().animate({
+            top: 10
+          }, 'slow');
+      } else if ($(window).width() >= 1400) {
+        $hero.animate({
+          top: 100
+          });
+        $about.show().animate({
+          top: 140
+        }, 'slow');
         } else {
-            $hero.animate({
-                top: 10
-            });
-            $about.show().animate({
-                top: 40
-            }, 'slow');
-        }
+        $hero.animate({
+          top: 10
+        });
+        $about.show().animate({
+          top: 40
+        }, 'slow');
+      }
     });
 
     // Hide about div. Show hero button
     $removeButton.on('click', function() {
-        $aboutButton.fadeIn();
-        $aboutP.css('margin-bottom', '10px');
+      $aboutButton.fadeIn();
+      $aboutP.css('margin-bottom', '10px');
 
-        //Hide the hero modal
-        $about.animate({
-            top: 1000
-        }, 'slow').fadeOut();
+      //Hide the hero modal
+      $about.animate({
+        top: 1000
+      }, 'slow').fadeOut();
 
-        // Set About panel back into place after hiding hero modal
-        if ($(window).width() <= 520) {
-            $hero.fadeIn();
-            $hero.animate({
-                top: 60
-            });
-            $about.hide();
-        } else if ($(window).width() >= 1400) {
-            $hero.animate({
-                top: 180
-            });
-        } else {
-            $hero.animate({
-                top: 100
-            });
-        }
-    });
-
-    // Top bar toggle show
-    $showNav.on('click', function() {
-        $(this).fadeOut('fast');
-        $hideNav.fadeIn();
-        $topBar.animate({
-            left: 0
+      // Set About panel back into place after hiding hero modal
+      if ($(window).width() <= 520) {
+        $hero.fadeIn();
+        $hero.animate({
+            top: 60
         });
-    })
+        $about.hide();
+      } else if ($(window).width() >= 1400) {
+        $hero.animate({
+            top: 180
+        });
+      } else {
+        $hero.animate({
+            top: 100
+        });
+      }
+    });
+  }();
+
+  // Top bar toggle show
+  var toggleNav = function () {
+    var $hideNav = $('.hide-nav'),
+        $showNav = $('.show-nav'),
+        $topBar = $('.top-bar');
+
+    $showNav.on('click', function() {
+      $(this).fadeOut('fast');
+      $hideNav.fadeIn();
+      $topBar.animate({
+        left: 0
+      });
+    });
 
     // Top bar toggle hide
     $hideNav.on('click', function() {
-        $(this).fadeOut();
-        $showNav.fadeIn();
-        $topBar.animate({
-            left: 3000
-        });
-    })
+      $(this).fadeOut();
+      $showNav.fadeIn();
+      $topBar.animate({
+        left: 3000
+      });
+    });
+  }();
 
     // PAGE NAV ARROWS <-- * -->
-    // RIGHT -->
+  var navArrows = function () {
+    var rightDisplay = $('.port-display, .contact-display'),
+        leftDisplay = $('.about-display, .port-display-left');
+
+    // Right -->
     $('.contact-page span, .port-page span').mouseover(function() {
-            $(this).fadeIn().css('color', '#fff');
-            $('.port-display, .contact-display').fadeIn();
-        })
-        .mouseout(function() {
-            $(this).css('color', 'rgba(255, 255, 255, 0.75)');
-            $('.port-display, .contact-display').fadeOut();
-        });
+      $(this).fadeIn().css('color', '#fff');
+      $(rightDisplay).fadeIn();
+    })
+    .mouseout(function() {
+      $(this).css('color', 'rgba(255, 255, 255, 0.75)');
+      $(rightDisplay).fadeOut();
+    });
 
     // LEFT <--
     $('.about-page span, .port-page-left span').mouseover(function() {
-            $(this).fadeIn().css('color', 'rgba(255, 255, 255, 0.75)');
-            $('.about-display, .port-display-left').fadeIn();
-        })
-        .mouseout(function() {
-            $(this).css('color', 'rgba(255, 255, 255, 0.65)');
-            $('.about-display, .port-display-left').fadeOut();
-        });
+      $(this).fadeIn().css('color', 'rgba(255, 255, 255, 0.75)');
+      $(leftDisplay).fadeIn();
+    })
+    .mouseout(function() {
+      $(this).css('color', 'rgba(255, 255, 255, 0.65)');
+      $(leftDisplay).fadeOut();
+    });
+  }();
 
-    //Set animation destination for flash arrow
-    var distance = function() {
-        var distanceDown;
+  //Set animation destination for flash arrow
+  var distance = function() {
+    var distanceDown;
 
-        if ($(window).width() > 1400) {
-            return distanceDown = 700;
-        } else {
-            return distanceDown = 550;
-        }
+    if ($(window).width() > 1400) {
+      return distanceDown = 700;
+    } else {
+      return distanceDown = 550;
     }
+  };
 
     // Flash arrow on portfolio page
+  var flashDownArroww = function () {
     $('.down-arrow').fadeIn('slow').animate({
-        top: distance()
+      top: distance()
     }).fadeOut('slow');
+  }();
 
-    // Have github icons fadeIn on hover of .work
+  // Have github icons fadeIn on hover of .work
+  var fadeInGitHub = function () {
     $('.work').hover(function() {
-        $('.work i').fadeIn();
-    })
+      $('.work i').fadeIn();
+    });
+  }();
 
-})
+});
